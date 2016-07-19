b1b5526c3f4bc815dcfbe4befcac910cf693203d
Adi Pascu <mail@adipascu.ro>
2016-07-19 18:33:15 +0300

    Hello world

--- entry.js
+++ entry.js
@@ -1,1 +1,15 @@
-document.write("It works.");
+//document.write("It works.");
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('example')
+// );
+
+
+ReactDOM.render(
+  React.createElement('h1', {}, "Hello, world!"),
+  document.getElementById('container')
+);
