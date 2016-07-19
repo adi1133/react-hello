1fac6fd4fdb4671370b7c4beb9c2ab70293d0b38
Adi Pascu <mail@adipascu.ro>
2016-07-19 20:41:50 +0300

    StringList component

--- entry.js
+++ entry.js
@@ -14,12 +14,16 @@ const addItem = () => {
 	list = list.concat(Math.floor(Math.random() * 100))
 	render()
 }
+const StringList = React.createClass({
+	render: () => React.DOM.ul({ key: 'list' }, list.map((item, pos) => React.createElement('li', { key: pos }, item))),
+	componentWillMount: () => console.log("mounting list")
+})
 const render = () => {
 	ReactDOM.render(
-		React.DOM.div({}, [
-			React.DOM.h1({ key: 'title' }, "Hello, world!"),
+		React.DOM.div({ data: list }, [
+			React.DOM.h1({ key: 'title' }, "Hello, world!" + list),
 			React.DOM.p({ key: 'p' }, "Paragraph"),
-			React.DOM.ul({ key: 'list' }, list.map((item, pos) => React.createElement('li', { key: pos }, item))),
+			React.createElement(StringList, { key: 'list' }),
 			buttonActive ? React.DOM.button({ onClick: addItem, key: 'button' }, "hello") : undefined
 
 		]),
