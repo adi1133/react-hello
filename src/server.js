5eac067f4981b2984693ad03f4067e76feb2e1ac
Adrian Pascu <mail@adipascu.ro>
2016-10-01 23:28:03 -0700

    Fix asset packaging using file-loader

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ import {port} from './config'
 
 const server = express();
 
-server.use(express.static(path.join(__dirname, 'public')));
+server.get('/', (req, res) => res.sendFile(path.join(__dirname, require('file-loader!./client/index.html'))));
+server.get('/bundle.js', (req, res) => res.sendFile(path.join(__dirname, 'public', 'main.js')));
 
 server.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}/`);
